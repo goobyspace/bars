271c0e594d6f69587826791fa859f798b0f66111
Gooby <gooby@gooby.space>
2026-09-21 10:39:45 +0200

    flight data

--- utils/FlightPathData.lua
+++ utils/FlightPathData.lua
@@ -6,6 +6,7 @@ core.flightPathData = {
         ["Rut'theran Village, Teldrassil" .. "\031" .. "Auberdine, Darkshore"] = 85.900,
     },
     Horde = {
+        ["Crossroads, The Barrens" .. "\031" .. "Thunder Bluff, Mulgore"] = 182.652,
         ["Orgrimmar, Durotar" .. "\031" .. "Crossroads, The Barrens"] = 110.620,
         ["Thunder Bluff, Mulgore" .. "\031" .. "Orgrimmar, Durotar"] = 207.610,
     },
